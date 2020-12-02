02aadb292ac9c4dec8493a91c447d6c86f14b536
Juan Carlos Farah <juancarlos.farah@epfl.ch>
2020-12-02 14:58:16 +0100

    fix: do not get actions in standalone mode
    closes #16

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -12,7 +12,6 @@ import { withTranslation } from 'react-i18next';
 import { ReactComponent as Logo } from '../../resources/logo.svg';
 import { toggleSideMenu } from '../../actions';
 import { DRAWER_WIDTH, LOGO_SIZE } from '../../config/constants';
-import { addQueryParamsToUrl } from '../../utils/url';
 
 const styles = (theme) => ({
   grow: {
@@ -62,50 +61,13 @@ class Header extends Component {
     }).isRequired,
     dispatchToggleSideMenu: PropTypes.func.isRequired,
     showSideMenu: PropTypes.bool.isRequired,
-    appInstanceId: PropTypes.string,
-    spaceId: PropTypes.string,
   };
 
-  static defaultProps = {
-    appInstanceId: null,
-    spaceId: null,
-  };
+  static defaultProps = {};
 
   handleToggleSideMenu = (open) => () => {
     const { dispatchToggleSideMenu } = this.props;
     dispatchToggleSideMenu(open);
-  };
-
-  renderAppInstanceLink = () => {
-    const { appInstanceId, t, classes } = this.props;
-    if (!appInstanceId) {
-      return (
-        <a
-          href={addQueryParamsToUrl({
-            appInstanceId: '6156e70ab253020033364411',
-          })}
-          className={classes.link}
-        >
-          {t('Use Sample App Instance')}
-        </a>
-      );
-    }
-    return <div />;
-  };
-
-  renderSpaceLink = () => {
-    const { spaceId, t, classes } = this.props;
-    if (!spaceId) {
-      return (
-        <a
-          href={addQueryParamsToUrl({ spaceId: '5b56e70ab253020033364411' })}
-          className={classes.link}
-        >
-          {t('Use Sample Space')}
-        </a>
-      );
-    }
-    return <div />;
   };
 
   render() {
@@ -122,9 +84,6 @@ class Header extends Component {
           <Typography variant="h6" color="inherit" className={classes.grow}>
             {t('Radiating Charge')}
           </Typography>
-
-          {this.renderAppInstanceLink()}
-          {this.renderSpaceLink()}
 
           {!showSideMenu && (
             <IconButton
