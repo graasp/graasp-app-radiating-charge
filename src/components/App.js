368ec07b44ce53364ad4c368d0b9dfc40b9be8b7
pyphilia <pyphilia@gmail.com>
2023-04-26 14:45:26 +0300

    refactor: use useTranslation

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import React, { useEffect } from 'react';
 import qs from 'qs';
-import { withTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import { Context, PermissionLevel } from '@graasp/sdk';
 import { useLocalContext } from '@graasp/apps-query-client';
-import PropTypes from 'prop-types';
 import StudentMode from './modes/student/StudentMode';
 import { DEFAULT_LANG } from '../config/settings';
 import TeacherMode from './modes/teacher/TeacherMode';
 
-export const App = ({ i18n }) => {
+export const App = () => {
+  const { i18n } = useTranslation();
   const { data: context } = useLocalContext();
 
   const handleChangeLang = (lang) => {
@@ -26,9 +26,11 @@ export const App = ({ i18n }) => {
     if (newLang !== i18n.language) {
       handleChangeLang(newLang || DEFAULT_LANG);
     }
-  }, [context?.lang]);
+  }, [context]);
 
+  // eslint-disable-next-line react/destructuring-assignment
   const view = context?.context;
+  // eslint-disable-next-line react/destructuring-assignment
   const permission = context?.permission;
   switch (view) {
     // show teacher view when in producer (educator) mode
@@ -46,12 +48,4 @@ export const App = ({ i18n }) => {
   }
 };
 
-App.propTypes = {
-  i18n: PropTypes.shape({
-    defaultNS: PropTypes.string,
-    changeLanguage: PropTypes.func,
-    language: PropTypes.string,
-  }).isRequired,
-};
-
-export default withTranslation()(App);
+export default App;
